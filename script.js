5e1697f8fc06fdb5be0f9b68cba6ef9efb47b141
Gaurang Tiwari <gaurangtiwari22@gmail.com>
2026-01-13 18:21:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,24 +14,25 @@ canvas.height = innerHeight;
 let p = [];
 for (let i = 0; i < 70; i++) {
   p.push({
-    x: Math.random()*canvas.width,
-    y: Math.random()*canvas.height,
-    r: Math.random()*2+1,
-    dx: Math.random()*0.4,
-    dy: Math.random()*0.4
+    x: Math.random() * canvas.width,
+    y: Math.random() * canvas.height,
+    r: Math.random() * 2 + 1,
+    dx: Math.random() * 0.4,
+    dy: Math.random() * 0.4
   });
 }
 
 function animate() {
-  ctx.clearRect(0,0,canvas.width,canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = 'rgba(255,255,255,0.4)';
   p.forEach(a => {
     ctx.beginPath();
-    ctx.arc(a.x,a.y,a.r,0,Math.PI*2);
+    ctx.arc(a.x, a.y, a.r, 0, Math.PI * 2);
     ctx.fill();
-    a.x+=a.dx; a.y+=a.dy;
-    if(a.x>canvas.width)a.x=0;
-    if(a.y>canvas.height)a.y=0;
+    a.x += a.dx;
+    a.y += a.dy;
+    if (a.x > canvas.width) a.x = 0;
+    if (a.y > canvas.height) a.y = 0;
   });
   requestAnimationFrame(animate);
 }
