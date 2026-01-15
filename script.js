d90787415cd5a6b3295640b452ab1dd84f663c16
Gaurang Tiwari <gaurangtiwari22@gmail.com>
2026-01-15 20:17:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,14 @@
-function scrollToSection(id) {
-  document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
-}
-
-function openModal(type) {
-  const frame = document.getElementById('modalFrame');
-
-  if (type === 'sales')
-    frame.src = 'projects/sales.csv.xlsx';
-  if (type === 'student')
-    frame.src = 'projects/Gaurang%20Tiwari%20–%20Student%20Performance%20Dashboard.xlsx';
-  if (type === 'sql')
-    frame.src = 'projects/SQL%20Project.docx';
-
-  document.getElementById('modal').style.display = 'block';
-}
-
 function openImage(src) {
-  const frame = document.getElementById('modalFrame');
-  frame.src = src;
-  document.getElementById('modal').style.display = 'block';
+  const modal = document.getElementById('modal');
+  const img = document.getElementById('modalContent');
+  img.src = src;
+  modal.style.display = 'flex';
 }
 
 function openPDF(src) {
-  const frame = document.getElementById('modalFrame');
-  frame.src = src;
-  document.getElementById('modal').style.display = 'block';
+  window.open(src, '_blank');
 }
 
 function closeModal() {
   document.getElementById('modal').style.display = 'none';
-  document.getElementById('modalFrame').src = '';
 }
